555b07777cb63105ffd9ba3e9b2242e522e87a37
Michael_Schrijvers_Thuis <michael.10330@hotmail.com>
2024-10-30 16:10:41 +0100

    Added functioning hamburgermenu

--- payment-landing-page/payment-landing-page/script.js
+++ payment-landing-page/payment-landing-page/script.js
@@ -1,1 +1,18 @@
 // write your JavaScript here
+// var click = document.getElementById("function");
+// click.addEventListener
+
+
+
+
+function hamburgerMenu() {
+    var links = document.getElementById("mobilelinks");
+    var content = document.getElementById("content")
+    if (links.style.display === "block") {
+        content.style.display = "flex"
+        links.style.display = "none"
+    } else {
+        links.style.display = "block"
+        content.style.display = "none"
+    }
+}
